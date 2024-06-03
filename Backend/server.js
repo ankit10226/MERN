9143951b0b3c860fd0d0afcc3638a06a68c90e05
Ankit Singh <ankit10226.as@gmail.com>
2024-06-03 23:12:25 +0530

    mongo

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,18 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect(
+    "mongodb+srv://rompiekiller:SQWvOHKJBbnIDdL7@cluster0.rgwx4qk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+).then(
+    ()=>{
+        console.log('Connected');
+    }
+).catch(
+    ()=>{
+        console.log('Failed');
+    }
+)
+
 app.get('/',(req,res)=>{
     res.json({mssg:"Welcome!"})
 })
